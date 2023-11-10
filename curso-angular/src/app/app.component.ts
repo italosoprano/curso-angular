7b68e5a163f1a84b9e97d65dacc23ac998817d13
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-11-10 11:16:47 -0400

    update

--- curso-angular/src/app/app.component.ts
+++ curso-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FirstComponentComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
