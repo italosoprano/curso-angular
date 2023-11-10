d0f30b845d018edc074dbf406cee6e3a064f0a00
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-11-10 00:14:44 -0400

    update

---
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-first-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.css'
+})
+export class FirstComponentComponent {
+
+}
